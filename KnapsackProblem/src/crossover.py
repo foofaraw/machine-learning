e5546f197e5d8d2cf701b456d32a17dc15496883
Jarek Danielak <danielak.jarek@gmail.com>
2020-03-20 14:24:20 +0100

    final

--- KnapsackProblem/src/crossover.py
+++ KnapsackProblem/src/crossover.py
@@ -1,10 +1,10 @@
 import numpy as np
-from Models.Consts import CROSSOVER_POINT
 from Models.Individual import Individual
 
 def crossover(parent1, parent2, crossover_rate):
     if (np.random.uniform(0, 1) < crossover_rate):
-        child_items = parent1.items[:CROSSOVER_POINT] + parent2.items[-(len(parent1.items) - CROSSOVER_POINT):]
+        crossover_point = np.random.randint(1, len(parent1.items) - 1)
+        child_items = parent1.items[:crossover_point] + parent2.items[-(len(parent1.items) - crossover_point):]
         return Individual(child_items, parent1.task)
     else:
         return parent1
--- KnapsackProblem/src/main.py
+++ KnapsackProblem/src/main.py
@@ -9,18 +9,20 @@ from Models.Population import Population
 from Models.Consts import *
 
 def main():
-    generate(NUMBER_OF_ITEMS, MAX_WEIGHT, MAX_SIZE, FILE_NAME)
+    # generate(NUMBER_OF_ITEMS, MAX_WEIGHT, MAX_SIZE, FILE_NAME)
     task = read(FILE_NAME)
-    population = init_population(task, POPULATION_SIZE)
 
-    start = timer()
-    get_best_from_population(population)    
-    end = timer()
-    print(end - start)
+    for j in range(5):
+        print('########## NEW RUN ##########')
+        population = init_population(task, POPULATION_SIZE)
+        start = timer()
+        best = get_best_from_population(population)
+        end = timer()
+        print('# Execution time: ' + str(end - start))
     
 def get_best_from_population(population):
     val_sum = 0
-    for _ in range(ITERATIONS):
+    for i in range(ITERATIONS):
         new_population = Population()
         while new_population.population_size < POPULATION_SIZE:
             parent1 = tournament(population, TOURNAMENT_SIZE)
@@ -29,8 +31,8 @@ def get_best_from_population(population):
             mutate(child, MUTATION_RATE)
             new_population.add_individual(child)
         population = new_population
-        val_sum += population.get_best_individual().evaluate()
-    print('Average best: ' + str(int(val_sum / ITERATIONS)))
+        best_in_generation = population.get_best_individual().evaluate()
+        print('Best in ' + str(i + 1) + ' generation: ' + str(best_in_generation))
     return population.get_best_individual()
 
 main()
