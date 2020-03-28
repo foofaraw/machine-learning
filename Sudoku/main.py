e998f3689f294e40dac4805fe705289625a98564
Jarek Danielak <danielak.jarek@gmail.com>
2020-03-28 09:06:31 +0100

    backtracking

--- Sudoku/main.py
+++ Sudoku/main.py
@@ -6,7 +6,7 @@ from solve import *
 def main():
     data = np.genfromtxt('Data/Sudoku.csv', delimiter=';', names=True, dtype=('>i4', '>i4', '|U81', '|U81'))
     sudoku = Sudoku(data[0])
-    res = is_valid(sudoku.solution, 5, 0, 2)
+    res = solve(sudoku.grid)
     print(res)
 
 main()
--- Sudoku/solve.py
+++ Sudoku/solve.py
@@ -1,30 +1,48 @@
 import numpy as np
 
+def solve(grid):
+    find = find_empty(grid)
+    if not find:
+        return True
+    else:
+        row, col = find
+
+    for i in range(1,10):
+        if is_valid(grid, i, row, col):
+            grid[row][col] = i
+
+            if solve(grid):
+                return True
+
+            grid[row][col] = 0
+
+    return False    
+
 def find_empty(grid):
-    for i in range(grid.size):
-        for j in range(grid[0].size):
+    for i in range(9):
+        for j in range(9):
             if grid[i][j] == 0:
                 return (i, j)
     return None
 
-def is_valid(grid, digit, x, y):
+def is_valid(grid, digit, row, col):
     # Check row
     for i in range(9):
-        if grid[x][i] == digit and y != i:
+        if grid[row][i] == digit and col != i:
             return False
 
     # Check column
     for i in range(9):
-        if grid[i][y] == digit and x != i:
+        if grid[i][col] == digit and row != i:
             return False
 
     # Check box
-    box_x = y // 3
-    box_y = x // 3
+    box_x = col // 3
+    box_y = row // 3
 
     for i in range(box_y * 3, box_y * 3 + 3):
         for j in range(box_x * 3, box_x * 3 + 3):
-            if grid[i][j] == digit and (i, j) != (x, y):
+            if grid[i][j] == digit and (i, j) != (row, col):
                 return False
 
     return True
