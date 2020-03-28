462df0646d16688e3a6b898908e4a14713afee52
Jarek Danielak <danielak.jarek@gmail.com>
2020-03-28 13:00:32 +0100

    simple backtracking finished

--- Sudoku/main.py
+++ Sudoku/main.py
@@ -1,18 +1,34 @@
 import numpy as np
+import matplotlib.pyplot as plt
 from timeit import default_timer as timer
+
 from Models.Sudoku import Sudoku
 from solve import *
 
 
 def main():
     data = np.genfromtxt('Data/Sudoku.csv', delimiter=';', names=True, dtype=('>i4', '>i4', '|U81', '|U81'))
-    sudoku = Sudoku(data[0])
+    sudokus = [Sudoku(i) for i in data]
 
-    start = timer()
-    solve(sudoku.grid)
-    end = timer()
+    times = []
+    sudokus = sudokus[:5]
+    for sudoku in sudokus:
+        start = timer()
+        solve(sudoku.grid)
+        end = timer()
+        times.append(end - start)
 
-    print('Solved: ' + str(sudoku.is_solved()))
-    print('Solution time: ' + str(end - start))
+    x = np.linspace(1, len(sudokus), len(sudokus))
+    y = times
+    for xx, yy in zip(x, y):
+        label = '{:.2f}'.format(yy)
+        plt.annotate(label, (xx,yy), textcoords='offset points', xytext=(0,10), ha='center')
+
+    plt.scatter(x, y)
+    plt.xticks(np.arange(min(x), max(x) + 1, 1))
+    plt.title('Time to solve')
+    plt.xlabel('Puzzle no.')
+    plt.ylabel('Time (s)')
+    plt.show()
 
 main()
--- Sudoku/solve.py
+++ Sudoku/solve.py
@@ -7,7 +7,7 @@ def solve(grid):
     else:
         row, col = find
 
-    for i in range(1,10):
+    for i in range(1, 10):
         if is_valid(grid, i, row, col):
             grid[row][col] = i
 
@@ -25,24 +25,23 @@ def find_empty(grid):
                 return (i, j)
     return None
 
-def is_valid(grid, digit, row, col):
-    # Check row
+def is_valid(grid, num, row, col):
     for i in range(9):
-        if grid[row][i] == digit and col != i:
+        # check row
+        if grid[row][i] == num and col != i:
             return False
 
-    # Check column
-    for i in range(9):
-        if grid[i][col] == digit and row != i:
+        # check column
+        if grid[i][col] == num and row != i:
             return False
 
     # Check box
-    box_x = col // 3
-    box_y = row // 3
+    box_x = (col // 3) * 3
+    box_y = (row // 3) * 3
 
-    for i in range(box_y * 3, box_y * 3 + 3):
-        for j in range(box_x * 3, box_x * 3 + 3):
-            if grid[i][j] == digit and (i, j) != (row, col):
+    for i in range(box_y, box_y + 3):
+        for j in range(box_x, box_x + 3):
+            if grid[i][j] == num and (i, j) != (row, col):
                 return False
 
     return True
